0c0958c40d6e8ac05d23332a120a3e48cf0e6df0
Anmolgoel29 <88969348+Anmolgoel29@users.noreply.github.com>
2023-11-21 02:07:21 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,11 @@
+let btn=document.querySelectorAll(".menu p");
+
+Array.from(btn).forEach((elem)=>{
+    elem.addEventListener("click",()=>{
+        // console.log(elem.innerHTML.trim());
+        if(elem.innerHTML.trim()=='PLAY VS CPU'){
+            window.location.href="cpu.html";
+        }
+    });
+});
+
